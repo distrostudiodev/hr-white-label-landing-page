0f3ef4255fb1e90d3a9ce6b4ccc9c175dfee88ad
sulenq <fatwalinovera@gmail.com>
2025-02-02 12:40:53 +0700

    fix routing

--- src/constant/routes.ts
+++ src/constant/routes.ts
@@ -1,9 +1,4 @@
-import AboutUsPage from "@/pages/AboutUsPage";
-import ProductsPage from "@/pages/ProductsPage";
 import RootPage from "@/pages/RootPage";
-import ServicesPage from "@/pages/ServicesPage";
-import WorkDetail from "@/pages/WorkDetail";
-import WorksPage from "@/pages/WorksPage";
 import { createElement } from "react";
 import { PathRouteProps } from "react-router-dom";
 
